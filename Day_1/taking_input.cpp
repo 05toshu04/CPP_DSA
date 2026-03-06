81d1edc3bc9aade155b6687e9a1a750f7a1a226e
Toshu <tussygoswami01@gmail.com>
2026-03-06 18:29:44 +0530

    taking input

---
+++ Day_1/taking_input.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int marks;
+    cout<<"Enter a marks: "<<endl;
+    cin>>marks; 
+    cout<<"marks of rahul are : "<<marks<<endl;
+    return 0;
+}
