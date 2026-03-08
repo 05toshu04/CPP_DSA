184eeaf55f5a596d9290a9bf5acddee6a98ad620
tushar goswami <tussygoswami01@gmail.com>
2026-03-08 12:11:17 +0530

    if-else condition, nested if else, else if condition

---
+++ Day_2/if-else.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+//     int age;
+//     cout<<"enter your age"<<endl;
+//     cin>>age;
+
+//  if-else condition
+//     if(age>=18){
+//         cout<<"eligible to vote"<<endl;
+
+//     }
+//     else{
+//         cout<<"not eligible to vote "<<endl;
+//     }
+
+
+//nested if-else condition
+
+
+// int marks;
+// cout<<"enter your marks"<<endl;
+// cin>>marks;
+
+// if (marks>=80){
+// cout<<"grade A"<<endl;
+
+
+// }
+// else {
+
+// if(marks>=60){
+//     cout<<"grade B"<<endl;
+// }
+
+// else {
+
+// if(marks>=40){
+//     cout<<"grade C"<<endl;
+// }
+
+// else {
+//     cout<<"fail"<<endl;
+// }
+
+// }
+
+// }
+
+
+
+// else if condition
+
+int marks;
+
+cout<<"enter marks"<<endl;
+cin>>marks;
+
+if(marks>=80){
+    cout<<"grade A"<<endl;
+}
+else if(marks>=60){
+    cout<<"grade B"<<endl;
+
+}
+else if(marks >=40){
+    cout<<"grade C"<<endl;
+}
+else {
+    cout<<"fail"<<endl;
+}
+
+
+
+
+}
+
