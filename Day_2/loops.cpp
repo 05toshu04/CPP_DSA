f774526a68bcd3740d4229ad28e8ceacfebf20e0
tushar goswami <tussygoswami01@gmail.com>
2026-03-11 15:55:56 +0530

    loops in cpp

---
+++ Day_2/loops.cpp
@@ -0,0 +1,25 @@
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+    
+
+for(int i=1;i<=10;i++){
+    cout<<"hello"<<endl;
+}
+
+
+}
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    for (int i=1;i<=10;i++){
+       cout<<"2 * "<<i<< "="<< 2*i<<endl;
+    }
+
+
+}
+
