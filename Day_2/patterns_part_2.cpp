5298fb07086a64f56c4cb8a6136d8fd5453362cc
tushar goswami <tussygoswami01@gmail.com>
2026-03-13 10:56:05 +0530

    patterns part 2 , hollow rectangle, half pyramid , numeric half pyramid

---
+++ Day_2/patterns_part_2.cpp
@@ -0,0 +1,137 @@
+// hollow rectangle 
+// * * * * * 
+// *       *
+// * * * * *
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+// int row_value;
+// int col_value;
+// cin>>row_value;
+// cin>>col_value;
+
+// for(int i=0;i<row_value;i++){
+
+// if(i==0 || i==row_value - 1){
+
+// for(int j=0;j<col_value;j++){
+
+
+//     cout<<"* " ;
+// }
+
+// }
+
+// else {
+
+// cout<<"* ";
+
+// for(int k=0;k<col_value-2;k++){
+//     cout<<"  ";
+// }
+
+// cout<<"* ";
+
+// }
+
+
+// cout<<endl;
+// }
+
+
+
+
+
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+// int row_count;
+// cin>>row_count;
+// int n=0;
+// for(int row=0;row<row_count;row++){
+
+
+
+// for(int col=0;col<=n;col++){
+
+// cout<<"* ";
+
+// }
+
+// n=n+1;
+
+// cout<<endl;
+
+
+// }
+
+ 
+
+// * * * * * *
+// * * * * *
+// * * * *
+// * * * 
+// * *
+// *
+
+
+// int col_value;
+// int row_value;
+
+// cin>>row_value;
+// cin>>col_value;
+
+// for(int row=0;row<row_value;row++){
+
+
+
+//  for(int col=0;col<col_value;col++){
+
+//     cout<<"* ";
+//  }
+//  col_value=col_value-1;
+//  cout<<endl;
+
+// }
+
+
+
+
+
+
+
+
+for(int row=1;row<=5;row++){
+
+
+for(int col=1;col<=row;col++){
+   cout<<col;
+   cout<<" ";
+}
+
+cout<<endl;
+
+
+}
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+    
