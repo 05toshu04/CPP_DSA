973b5a0ae2653fde72e3b83aec1b3f8adb5b5217
tushar goswami <tussygoswami01@gmail.com>
2026-03-13 11:51:29 +0530

    inverted pyramid pattern

---
+++ Day_2/Patterns_questions/inverted_pyramid.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int n=0;
+
+for(int row =0;row<6;row++){
+
+
+for(int k=0;k<n;k++){
+  cout<<"  ";
+}
+
+
+
+for(int j=6;j>row;j--){
+
+
+    cout<<"* ";
+    cout<<"  ";
+}
+
+cout<<endl;
+n=n+1;
+
+
+}
+
+
+
+
+
+}
