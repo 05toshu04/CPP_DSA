6e635c5d63c73f2f2dbe8116fb9b8cdff6f2b641
tushar goswami <tussygoswami01@gmail.com>
2026-03-21 16:52:52 +0530

    start function topic

---
+++ Day_5-functions/basic_f.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void printname(){
+    int n;
+    cout<<"enter how many times you want to print name"<<endl;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cout<<"Tushar Goswami"<<endl;
+    }
+    
+}
+int main(){
+    printname();
+    printname();
+
+    
+}
